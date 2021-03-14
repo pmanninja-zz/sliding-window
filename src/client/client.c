d274ce390024a853c79bf864a03b8eabc6f6c32a
Nwabunor Jeff Onwuanyi <elite057@gmail.com>
2021-03-14 17:16:03 -0600

    file transfer working

--- src/client/client.c
+++ src/client/client.c
@@ -14,35 +14,159 @@ Author(s): Nwabunor Onwuanyi , Prosper ibhamawu
 #include <sys/socket.h>
 #include <arpa/inet.h>
 
+#define IP_PROTOCOL 0
+#define IP_ADDRESS "127.0.0.1" // localhost
+#define PORT_NO 15050
+#define BUFFERLEN 32
+#define cipherKey 'S'
+#define sendrecvflag 0
+#define nofile "File Not Found!"
+
+void clearBuf(char* b);
+int sendFile(FILE* fp, char* buf, int s);
+int recvFile(char* buf, int s);
+char Cipher(char ch);
 
 int main() {
-  char  packet[100];
-  double port;
-  printf("%s","Enter your transmision: ");
-  fgets(packet, 100, stdin);
-  printf("%s","Enter port number: ");
-  scanf("%lf", &port);
+  int sockfd, nbytes, len, selection, port;
+  char address[9];
+  struct sockaddr_in servaddr;
+  int addrlen = sizeof(servaddr);
+  char buffer[BUFFERLEN];
+  FILE* fp;
 
-  struct sockaddr_in servaddr = {0};
+  printf("%s","Enter port number enter 2 to use default: ");
+  scanf("%d", &port);
 
+  servaddr.sin_family = AF_INET; // ipv4
 
-  int sockfd = socket(AF_INET, SOCK_DGRAM, 0);
+  // port number for what service gets the data that comes from client||host to network short (htons)
+  if(port == 2) {
+    servaddr.sin_port = htons(PORT_NO);
+  } else {
+    servaddr.sin_port = htons(port);
+  }
+  // host address||host to network long (htonl)
+    servaddr.sin_addr.s_addr = inet_addr(IP_ADDRESS);
 
-  if (sockfd == -1) {
-    perror("failed to create");
-    exit(EXIT_FAILURE);
+    // IPv4 socket made|| IPv4,UDP,protocol
+    sockfd = socket(AF_INET, SOCK_DGRAM, IP_PROTOCOL);
+
+    // if socket failed to be made output error and exit
+    if (sockfd < 0) {
+      perror("failed to create");
+      exit(EXIT_FAILURE);
+      } else {
+        printf("%s\n","_________________");
+        printf("%s\n","client is running");
+        printf("%s\n","-----------------");
+      }
+  START:
+  printf("send an option\n");
+  printf("1)Send single entry\n");
+  printf("2)Recive file entry\n");
+  printf("3)Send file entry\n");
+  printf("Enter a number: ");
+  scanf("%d",&selection);
+
+  if (selection == 1 ) {
+    goto SENDSINGLE;
+  } else if(selection == 2) {
+    goto RECIVEFILE;
+  } else if(selection == 3) {
+    goto SENDFILE;
+  } else {
+    printf("Select a valid number");
+    goto START;
   }
 
-  servaddr.sin_family = AF_INET;
-  servaddr.sin_port = htons(port);
-  servaddr.sin_addr.s_addr = INADDR_ANY;
-
-
-  int len = sendto(sockfd, (const char *)packet, strlen(packet), 0, (const struct sockaddr*)&servaddr, sizeof(servaddr));
+//________________________________Reciving a single terminal input block (e.g test.txt)__________________
+SENDSINGLE:
+printf("%s\n","Enter your transmision: ");
+fgets(buffer, BUFFERLEN, stdin);
+len = sendto(sockfd, (const char *)buffer, strlen(buffer), 0, (const struct sockaddr*)&servaddr, sizeof(servaddr));
   if (len == -1) {
     perror("failed to send");
   }
+  goto END;
+//_______________________________________________________________________________________________________
+//________________________________Reciving a file  block (e.g test.txt)__________________________________
+RECIVEFILE:
+  while (1) {
+    printf("\nPlease enter file name to receive:\n");
+    scanf("%s", buffer);
+    sendto(sockfd, buffer, BUFFERLEN, sendrecvflag, (struct sockaddr*)&servaddr, addrlen);
+    printf("\n---------Data Received---------\n");
 
+    while (1) {
+      // receive
+      clearBuf(buffer);
+      nbytes = recvfrom(sockfd,buffer,BUFFERLEN, sendrecvflag, (struct sockaddr*)&servaddr, &addrlen);
+      // process
+      if (recvFile(buffer, BUFFERLEN)) {
+        break;
+      }
+    }
+    printf("\n-------------------------------\n");
+    goto END;
+  }
+//_______________________________________________________________________________________________________
+
+  END:
   close(sockfd);
   return 0;
+}
+
+// function to clear buffer
+void clearBuf(char* b)
+{
+    int i;
+    for (i = 0; i < BUFFERLEN; i++)
+        b[i] = '\0';
+}
+
+// function for decryption
+char Cipher(char ch)
+{
+    return ch ^ cipherKey;
+}
+
+// function to receive file
+int recvFile(char* buf, int s)
+{
+    int i;
+    char ch;
+    for (i = 0; i < s; i++) {
+        ch = buf[i];
+        ch = Cipher(ch);
+        if (ch == EOF)
+            return 1;
+        else
+            printf("%c", ch);
+    }
+    return 0;
+}
+
+// function sending file
+int sendFile(FILE* fp, char* buf, int s) {
+    int i, len;
+    if (fp == NULL) {
+        strcpy(buf, nofile);
+        len = strlen(nofile);
+        buf[len] = EOF;
+        for (i = 0; i <= len; i++)
+            buf[i] = Cipher(buf[i]);
+        return 1;
+    }
+
+    char ch, ch2;
+    for (i = 0; i < s; i++) {
+        ch = fgetc(fp);
+        ch2 = Cipher(ch);
+        buf[i] = ch2;
+        if (ch == EOF)
+            return 1;
+    }
+    return 0;
+
 }
--- src/server/server.c
+++ src/server/server.c
@@ -14,33 +14,58 @@ Author(s): Nwabunor Onwuanyi , Prosper ibhamawu
 #include <arpa/inet.h>
 #include <sys/wait.h>
 #include <signal.h>
-
 #include <limits.h>
+#include <string.h>
 
-#define BUFFERLEN 1000   // how many pending connections queue will hold
+#define BUFFERLEN 32   // how many pending connections queue will hold
+#define IP_ADDRESS "127.0.0.1" // localhost
+#define PORT_NO 15050  // default port
+#define IP_PROTOCOL 0
+#define cipherKey 'S'
+#define sendrecvflag 0
+#define nofile "File Not Found!"
+
+void clearBuf(char* b);
+int sendFile(FILE* fp, char* buf, int s);
+int recvFile(char* buf, int s);
+char Cipher(char ch);
 
 int main() {
-  double port;
-  printf("Enter port number: ");
-  scanf("%lf", &port);
-  printf("hostname: %s\n", hostname);
+  int sockfd, nbytes, selection, n, port;
+  struct sockaddr_in servaddr;
+  int addrlen = sizeof(servaddr);
+  char buffer[BUFFERLEN];
+  FILE* fp;
+  socklen_t len = 0;
 
-  char buffer[BUFFERLEN] = {0};
-  struct sockaddr_in servaddr = {0};
+  printf("%s","Enter port number enter 2 to use default: ");
+  scanf("%d", &port);
 
-  int sockfd = socket(AF_INET, SOCK_DGRAM, 0);  // IPv4 socket made
+  servaddr.sin_family = AF_INET;  // ipv4
+
+  // port number for what service gets the data that comes from client||host to network short (htons)
+  if(port == 2) {
+    servaddr.sin_port = htons(PORT_NO);
+  } else {
+    servaddr.sin_port = htons(port);
+  }
+
+  // host address||host to network long (htonl)
+  servaddr.sin_addr.s_addr = htonl(INADDR_ANY);
+
+
+  // IPv4 socket made|| IPv4,UDP,protocol
+  sockfd = socket(AF_INET, SOCK_DGRAM, IP_PROTOCOL);
 
   // if socket failed to be made output error and exit
-  if (sockfd == -1) {
+  if (sockfd < 0) {
     perror("failed to create");
     exit(EXIT_FAILURE);
   }
 
-  servaddr.sin_family = AF_INET;
-  servaddr.sin_port = htons(port);
-  servaddr.sin_addr.s_addr = INADDR_ANY;
 
-  int reciv = bind(sockfd, (const struct sockaddr *)&servaddr, sizeof(servaddr)); // bind the made socket to reciv
+  // bind the made socket to reciv ||file descriptor socket(),my address,address length
+  int reciv = bind(sockfd, (const struct sockaddr *)&servaddr, sizeof(servaddr));
 
   // if socket failed to bind output error and exit
   if (reciv == -1) {
@@ -53,13 +78,120 @@ int main() {
     printf("%s\n","-----------------");
   }
 
-  socklen_t len = 0;
-  int n = recvfrom(sockfd, (char *)buffer, BUFFERLEN, MSG_WAITALL, 0, &len);
+  START:
+  printf("send an option\n");
+  printf("1)Recive single entry\n");
+  printf("2)Send file entry\n");
+  printf("Enter a number: ");
+  scanf("%d",&selection);
+
+  if (selection == 1 ) {
+    goto RECIVESINGLE;
+  } else if(selection == 2) {
+    goto SENDFILE;
+  } else if(selection == 3) {
+    goto RECIVEFILE;
+  } else {
+    printf("Select a valid number");
+    goto START;
+  }
+
+
+
+//________________________________Reciving a line form terminal block (e.g hi my name is)________________________
+  RECIVESINGLE:
+
+  n = recvfrom(sockfd, (char *)buffer, BUFFERLEN, MSG_WAITALL, 0, &len);
 
   printf("%s","recived transmision : " );
   printf("%s", buffer);
+  goto END;
+//_______________________________________________________________________________________________________________
 
+
+//________________________________Sending a file block (e.g testfile.txt)________________________________________
+  SENDFILE:
+  while(1) {
+    printf("\nWaiting for file name ....\n");
+    clearBuf(buffer);
+
+    nbytes = recvfrom(sockfd, buffer, BUFFERLEN, sendrecvflag, (struct sockaddr*)&servaddr, &addrlen);
+
+    fp = fopen(buffer,"r");
+    printf("\nFile Name Received: %s\n",buffer);
+    if(fp == NULL) {
+      printf("\nFile Open Failed!\n");
+    } else {
+      printf("\nFile Successfully Opened!\n");
+    }
+
+    while(1) {
+      if (sendFile(fp,buffer,BUFFERLEN)) {
+        sendto(sockfd,buffer,BUFFERLEN,sendrecvflag,(struct sockaddr*)&servaddr,addrlen);
+        break;
+      }
+      sendto(sockfd,buffer,BUFFERLEN,sendrecvflag,(struct sockaddr*)&servaddr,addrlen);
+      clearBuf(buffer);
+    }
+    if(fp != NULL) {
+      fclose(fp);
+      goto END;
+    }
+  }
+//______________________________________________________________________________________________________________
+
+  END:
   close(sockfd);
-
   return 0;
+}
+
+
+// function to clear buffer
+void clearBuf(char* b) {
+    int i;
+    for (i = 0; i < BUFFERLEN; i++)
+        b[i] = '\0';
+}
+
+// function sending file
+int sendFile(FILE* fp, char* buf, int s) {
+    int i, len;
+    if (fp == NULL) {
+        strcpy(buf, nofile);
+        len = strlen(nofile);
+        buf[len] = EOF;
+        for (i = 0; i <= len; i++)
+            buf[i] = Cipher(buf[i]);
+        return 1;
+    }
+
+    char ch, ch2;
+    for (i = 0; i < s; i++) {
+        ch = fgetc(fp);
+        ch2 = Cipher(ch);
+        buf[i] = ch2;
+        if (ch == EOF)
+            return 1;
+    }
+    return 0;
+
+}
+
+char Cipher(char ch) {
+    return ch ^ cipherKey;
+}
+
+int recvFile(char* buf, int s)
+{
+    int i;
+    char ch;
+    for (i = 0; i < s; i++) {
+        ch = buf[i];
+        ch = Cipher(ch);
+        if (ch == EOF)
+            return 1;
+        else
+            printf("%c", ch);
+    }
+    return 0;
 }
