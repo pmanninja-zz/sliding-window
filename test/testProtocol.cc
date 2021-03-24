d2f41bfb311f31f5d57daf64a2980d8b8babe741
Nwabunor Jeff Onwuanyi <elite057@gmail.com>
2021-03-24 14:50:59 -0600

    Update testProtocol.cc

--- test/testProtocol.cc
+++ test/testProtocol.cc
@@ -1,2 +1,31 @@
 #include "Protocol.h"
 #include "gtest/gtest.h"
+
+
+
+TEST(testProtocol, checksum) {
+  u_long sum = 0;
+  int count = 5;
+
+  while(count--) {
+    sum += MAX_FRAME_SIZE;
+    if (sum & 0xFFFF0000) {
+      sum &= 0xFFFF
+      sum++;
+    }
+  EXPECT_TRUE(sum >= 0);
+  EXPECT_FALSE(sum <= 0);
+}
+
+TEST(testProtocol, create_frame) {
+
+}
+TEST(testProtocol, create_ack) {
+
+}
+TEST(testProtocol, read_frame) {
+
+}
+TEST(testProtocol, read_ack) {
+
+}
